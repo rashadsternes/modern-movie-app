32bd08c883a322ca07b1415682e543cde716fd45
rashadsternes <nrsternes@gmail.com>
2025-07-02 12:55:48 -0500

    Made visual improvements

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,7 @@ const App = () => {
       <div className="pattern" />
       <div className="wrapper" >
         <header>
-          <img src="./hero-img.png" alt="Hero Banner"></img>
+          <img src="./movie-triposter.png" alt="Hero Banner"></img>
           <h1>Discover New <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
         
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
